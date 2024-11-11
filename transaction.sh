527296a2af7f8388d4488423656a5835268d023e
venkateshnpci <venkateshy4620@gmail.com>
2024-11-11 11:23:33 +0530

    Update transaction.sh

--- transaction.sh
+++ transaction.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Input parameters: account number, transaction type, and amount
-account_number=$1
-transaction_type=$2
-amount=$3
+account_number=$1000
+transaction_type=$2000
+amount=$300
 
 # Get the current timestamp
 timestamp= #todo
